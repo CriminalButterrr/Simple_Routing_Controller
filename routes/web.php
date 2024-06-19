b8fc3d116ebff20d0307f16d1065814ef4e9d318
CriminalButterrr <h.agwiking@gmail.com>
2024-06-19 22:36:38 +0800

    Created the routes, views, and controller files

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/aboutme', function () {
+    return view('aboutme');
+});
+
+Route::get('/hobbies', function () {
+    return view('hobbies');
+});
+
+Route::get('/skills', function () {
+    return view('skills');
+});
